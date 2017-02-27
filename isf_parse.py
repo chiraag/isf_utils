bbc0745472434d413dd7bf5d48a74140e4cdbf3b
Chiraag Juvekar <chiraag.juvekar@gmail.com>
2017-02-27 05:24:09 -0500

    Basic ISF viewer

---
+++ isf_parse.py
@@ -0,0 +1,135 @@
+import numpy as np
+import os.path
+
+""" Code based on http://codereview.stackexchange.com/questions/91032/parsing-oscilloscope-data-follow-up"""
+
+def _read_chunk(headerfile, delimiter):
+    """
+    Reads one chunk of header data. Based on delimiter, this may be a tag
+    (ended by " ") or the value of a tag (ended by ";").
+    """
+    prior_delimiter = None
+    chunk = []
+    while True:
+        c = headerfile.read(1)
+        if c != delimiter:
+            chunk.append(c)
+            if c == '"':
+                # switch delimiter to make sure to parse the whole string
+                # enclosed in '"'.
+                delimiter, prior_delimiter = c, delimiter
+        elif prior_delimiter:
+            # switch back delimiter
+            chunk.append(c)
+            delimiter, prior_delimiter = prior_delimiter, None
+        else:
+            return "".join(chunk)
+
+
+def _read_data(bfile, position, header):
+    """
+    Reads in the binary data as numpy array.
+    Apparently, there are only 1d-signals stored in .isf files, so a 1d-array
+    is read.
+    """
+    # determine the datatype from header tags
+    datatype = ">" if header["BYT_OR"] == "MSB" else "<"
+    if header["BN_FMT"] == "RI":
+        datatype += "i"
+    else:
+        datatype += "u"
+    # BYT_NR might be present with preceding header ":WFMPRE:BYT_NR"
+    nobytes = header.get("BYT_NR", header.get(":WFMPRE:BYT_NR", ""))
+    datatype += nobytes
+    assert len(datatype) >= 3
+
+    bfile.seek(position)
+    data = np.fromfile(bfile, datatype)
+    assert data.size == int(header["NR_PT"])
+
+    # calculate true values
+    offset = float(header["VPOS"])*float(header["VSCALE"])
+    data = data * float(header["YMULT"]) + float(header["YZERO"]) - offset
+
+    return data
+
+class Curve(object):
+    """
+    Reads one tektronix .isf file and returns a dictionary containing the header
+    dict and the data. All tags are keys in the header dict
+    """
+
+    def __init__(self, isf_file):
+        self.isf_file = isf_file
+        self.header = {}
+        self.data = np.array([])
+        self.parse_curve()
+
+
+    def parse_curve(self):
+        """
+        Reads one tektronix .isf file and returns a dictionary containing the header
+        dict and the data. All tags are keys in the header dict
+        """
+
+        extensions = set([".isf"])
+        if os.path.splitext(self.isf_file)[-1].lower() not in extensions:
+            raise ValueError("File type unkown.")
+
+        with open(self.isf_file, 'rb') as ifile:
+            # read header
+            while True:
+                name = _read_chunk(ifile, " ")
+                if name != ":CURVE":
+                    value = _read_chunk(ifile, ";")
+
+                    assert name not in self.header
+                    self.header[name] = value
+                else:
+                    # ":CURVE " is the last tag of the header, followed by
+                    # '#XYYY' with X being the number of bytes of YYY.
+                    # YYY is the length of the datastream following in bytes.
+                    value = ifile.read(2)
+                    y_str = ifile.read(int(value[-1]))
+                    value += y_str
+
+                    # the number of bytes might be present with or without the
+                    # preceding header ":WFMPRE:"
+                    nobytes = self.header.get("BYT_NR",
+                        self.header.get(":WFMPRE:BYT_NR", "0")
+                    )
+                    assert int(y_str) == int(self.header["NR_PT"]) * int(nobytes)
+                    self.header[name] = value
+                    currentposition = ifile.tell()
+                    break
+
+            assert self.header["ENCDG"] == "BINARY"
+
+            # read data as numpy array
+            self.data = _read_data(ifile, currentposition, self.header)
+        return
+
+class CurveSet(object):
+    def __init__(self, folder, names=[]):
+        self.name = folder
+        self.curves = {}
+        self.t = np.array([])
+        if not os.path.isdir(folder):
+            raise ValueError("Not a folder")
+        for filename in os.listdir(folder):
+            name, ext = os.path.splitext(filename)
+            if ext == '.isf':
+                channel = int(name[-1])-1
+                curvename = names[channel] if channel < len(names) else name
+                self.curves[curvename] = Curve(folder+'/'+filename)
+        if len(self.curves) != 0:
+            header = self.curves[self.curves.keys()[0]].header
+            nr_pt, tzero, tincr = [float(header[k]) for k in ['NR_PT', 'XZERO', 'XINCR']]
+            self.t = tzero + tincr*np.arange(nr_pt)
+
+    def downsample(self, tlim=None, max_points=10000):
+        tlim = (self.t[0], self.t[-1]) if tlim == None else tlim
+        nmin, nmax = np.searchsorted(self.t, tlim)
+        delta = (nmax-nmin)/min(nmax-nmin, max_points)
+        n = np.arange(nmin, nmax, delta)
+        return dict({'t':self.t[n]}, **{k:self.curves[k].data[n] for k in self.curves})
---
+++ isf_viewer.py
@@ -0,0 +1,62 @@
+from tornado import gen
+from tornado.ioloop import IOLoop
+
+from bokeh.server.server import Server
+from bokeh.application.handlers import FunctionHandler
+from bokeh.application import Application
+
+import numpy as np
+import isf_parse
+
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
+
+class BokehScope(object):
+    def __init__(self, curves, active):
+        self.io_loop = IOLoop.current()
+        self.bokeh_app = Application(FunctionHandler(self.modify_doc))
+        self.server = Server({'/': self.bokeh_app}, io_loop=self.io_loop)
+
+        self.colors = ['blue', 'red', 'green', 'magenta']
+        self.curves = curves
+        self.active = active
+        self.source = ColumnDataSource(data=self.curves.downsample())
+        self.busy = False
+        self.skip_update = False
+
+    def plot(self):
+        print('Opening Bokeh application on http://localhost:5006/')
+        self.server.start()
+        self.io_loop.add_callback(self.server.show, "/")
+        self.io_loop.start()
+
+    def modify_doc(self, doc):
+        plot = figure(plot_width=800, title="Waveforms",
+                tools="xpan,xwheel_zoom,xbox_zoom,undo, redo, reset")
+        for i, c in enumerate(self.active):
+            plot.line(x='t', y=c, color=self.colors[i], source=self.source)
+        doc.add_root(plot)
+
+        @gen.coroutine
+        def update():
+            self.source.data = self.curves.downsample(
+                    tlim=[plot.x_range.start, plot.x_range.end])
+            self.skip_update = True
+            self.busy = False
+
+        def change_callback(attr, old, new):
+            if not self.busy:
+                if not self.skip_update:
+                    self.busy = True
+                    doc.add_next_tick_callback(update)
+                else:
+                    self.skip_update = False
+        plot.x_range.on_change('end', change_callback)
+
+if __name__ == '__main__':
+    c=isf_parse.CurveSet('example', ['clk', 'cmd', 'dat'])
+    app = BokehScope(c, ['clk', 'cmd'])
+    app.plot()
+
+
+
